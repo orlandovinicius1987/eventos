8d0011aa789d1163cb6afd25f7bd99b5e06a9e12
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-10-16 00:31:42 -0300

    Split events and subevents

--- resources/js/views/Events.vue
+++ resources/js/views/Events.vue
@@ -5,30 +5,71 @@
         </div>
 
         <div class="row">
-            <div class="col-12">
-                <router-link to="/events/create" tag="div" class="btn btn-primary btn-sm m-1 pull-right">
-                    <i class="fa fa-plus"></i> novo evento
-                </router-link>
-            </div>
-        </div>
+            <div class="col-4">
+                <div class="row align-items-end">
+                    <div class="col-6">
+                        <h4 class="mb-0">Eventos</h4>
+                    </div>
+                    <div class="col-6">
+                        <router-link to="/events/create" tag="div" class="btn btn-primary btn-sm m-1 pull-right">
+                            <i class="fa fa-plus"></i> novo evento
+                        </router-link>
+                    </div>
+                </div>
 
-        <div class="row">
-            <div class="col-12">
-                <div class="card p-4">
-                    <table class="table table-sm table-hover table-borderless table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Nome</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="event in events">
-                                <td>{{ event.id }}</td>
-                                <td>{{ event.name }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card p-4">
+                            <table class="table table-sm table-hover table-borderless table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Nome</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="event in events">
+                                        <td>{{ event.id }}</td>
+                                        <td>{{ event.name }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-8">
+                <div class="row align-items-end">
+                    <div class="col-6">
+                        <h4 class="mb-0">Subeventos de whatever</h4>
+                    </div>
+                    <div class="col-6">
+                        <router-link to="/events/create" tag="div" class="btn btn-primary btn-sm m-1 pull-right">
+                            <i class="fa fa-plus"></i> novo evento
+                        </router-link>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card p-4">
+                            <table class="table table-sm table-hover table-borderless table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Nome</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="event in events">
+                                        <td>{{ event.id }}</td>
+                                        <td>{{ event.name }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
