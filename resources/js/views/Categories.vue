639ae29c3a1d256bc6b70f5d1a4b091b639b7efe
Orlando Vinícius Valença Ferreira da Silva <orlando.vinicius1987@gmail.com>
2018-10-18 15:32:48 +0000

    Categories

---
+++ resources/js/views/Categories.vue
@@ -0,0 +1,41 @@
+<template>
+ Categories
+
+</template>
+
+<script>
+import crud from './mixins/crud'
+import permissions from './mixins/permissions'
+import { mapMutations, mapState } from 'vuex'
+
+const serviceName = 'categories'
+
+export default {
+    mixins: [crud, events, permissions],
+
+    data() {
+        return {
+            serviceName: serviceName,
+        }
+    },
+
+    computed: {
+        ...mapState(serviceName, ['selectedEvent']),
+    },
+
+    methods: {
+        ...mapMutations(serviceName, ['selectEvent']),
+
+        isCurrent(event, selected) {
+            return event.id === selected.id
+        },
+    },
+
+    mounted() {
+        this.boot()
+    }
+}
+</script>
+
+<style>
+</style>
