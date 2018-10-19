d71f5b64a655e41ddbcc21d9e54e3bd6e0f9c578
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-10-19 18:38:35 -0300

    Fix page number

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use PragmaRX\Coollection\Package\Coollection;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Coollection::setRaiseExceptionOnNull(false);
     }
 
     /**
