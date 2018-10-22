cec83395c6e3fa519962f975cf9008ef728c4b09
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-10-22 20:17:15 -0300

    Filter events, subevents and invitations

--- app/Data/Repositories/Invitations.php
+++ app/Data/Repositories/Invitations.php
@@ -4,6 +4,7 @@ namespace App\Data\Repositories;
 
 use App\Data\Models\PersonInstitution;
 use App\Data\Models\Invitation as InvitationModel;
+use Illuminate\Database\Eloquent\Builder;
 
 class Invitations extends Repository
 {
@@ -11,4 +12,21 @@ class Invitations extends Repository
      * @var string
      */
     protected $model = InvitationModel::class;
+
+    protected function filterAllColumns(Builder $query, $text)
+    {
+        $query
+            ->join('person_institutions', 'person_institutions.id', '=', 'invitations.person_institution_id')
+            ->join('institutions', 'person_institutions.institution_id', '=', 'institutions.id')
+            ->join('people', 'person_institutions.person_id', '=', 'people.id')
+            ->join('roles', 'person_institutions.role_id', '=', 'roles.id')
+            ->where(function ($query) use ($text) {
+                $query->orWhere('institutions.name', 'ilike', "%{$text}%");
+                $query->orWhere('people.name', 'ilike', "%{$text}%");
+                $query->orWhere('roles.name', 'ilike', "%{$text}%");
+            });
+
+        return $query;
+    }
+
 }
