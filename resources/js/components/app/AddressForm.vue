d44621caafdb6e1626bcefe858eb1883d4237255
BMasquio <brunomasquio@gmail.com>
2018-10-29 12:52:48 +0000

    Working on Addresses

---
+++ resources/js/components/app/AddressForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <label :for="name" class="mb-0 mt-4">{{ label }}</label>
+
+        <input
+            v-bind:value="value"
+            v-on:input="$emit('input', $event.target.value)"
+            class="form-control"
+            :id="name"
+            :type="type"
+            :required="required"
+            :dusk="dusk"
+        >
+
+        <small class="text-danger" v-if="form.errors.has(name)" >
+            {{ form.errors.get(name) }}
+        </small>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['value', 'name', 'label', 'required', 'form', 'type', 'dusk'],
+}
+</script>
