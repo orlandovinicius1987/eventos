8e5c5a48a9b38319f9f7792bf7d719ba5e343285
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-11-08 18:25:02 -0200

    Finished uploading photo

--- app/Data/Repositories/People.php
+++ app/Data/Repositories/People.php
@@ -2,6 +2,7 @@
 
 namespace App\Data\Repositories;
 
+use Intervention\Image\Facades\Image;
 use App\Data\Models\Person as PersonModel;
 
 class People extends Repository
@@ -10,4 +11,23 @@ class People extends Repository
      * @var string
      */
     protected $model = PersonModel::class;
+
+    private function dummyPhotoAddress()
+    {
+        return public_path('img/no-image.jpg');
+    }
+
+    private function extractImage($photo)
+    {
+        return substr($photo, strpos($photo, ';') + 1);
+    }
+
+    public function getPhotoResponse($id)
+    {
+        $photo = $this->findById($id)->photo;
+
+        return Image::make(
+            !$photo ? $this->dummyPhotoAddress() : $this->extractImage($photo)
+        )->response();
+    }
 }
