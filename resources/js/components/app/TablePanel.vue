64011cf7686ffce2215b7ed52085f023714495b6
Breno Trengrouse <brenot@gmail.com>
2018-11-09 18:32:42 +0000

    Styling - Cards Style 001

--- resources/js/components/app/TablePanel.vue
+++ resources/js/components/app/TablePanel.vue
@@ -1,46 +1,50 @@
 <template>
     <div class="card shadow-sm">
-        <div class="row align-items-end">
-            <div class="col-12">
-                <div class="p-2 mb-2 mt-4">
-                    <h4 class="mb-0">{{ title }}</h4>
-                </div>
-            </div>
-
-            <div v-if="perPage" class="col-12 card-filters bg-secondary">
-                <div class="row">
-                    <div class="col-6">
-                        <input
-                            class="form-control form-control-sm"
-                            :value="filterText"
-                            @input="$emit('input-filter-text', $event)"
-                            placeholder="filtrar"
-                        >
-                    </div>
-
-                    <div class="col-2 p-0">
-                        <app-per-page
-                            :value="perPage"
-                            @input="$emit('set-per-page', $event)"
-                        ></app-per-page>
-                    </div>
-
-                    <div class="col-4 text-right">
-                        <router-link
-                            v-if="addButton"
-                            :to="addButton.uri"
-                            tag="div"
-                            class="btn btn-primary btn-sm pull-right"
-                            :disabled="addButton.disabled"
-                            dusk="createEventButton"
-                        >
-                            <i class="fa fa-plus"></i>
-                        </router-link>
-
-                        <slot name="buttons"></slot>
+        <div class="align-items-end card-header">
+            <div class="row">
+                <div class="col-12">
+                    <div class="p-2 mb-2 ">
+                        <h4 class="mb-0">{{ title }}</h4>
                     </div>
                 </div>
+
+                <div v-if="perPage" class="col-12 card-filters bg-filters py-2">
+                    <div class="row">
+                        <div class="col-6">
+                            <input
+                                    class="form-control"
+                                    :value="filterText"
+                                    @input="$emit('input-filter-text', $event)"
+                                    placeholder="filtrar"
+                            >
+                        </div>
+
+                        <div class="col-2 p-0">
+                            <app-per-page
+                                    :value="perPage"
+                                    @input="$emit('set-per-page', $event)"
+                            ></app-per-page>
+                        </div>
+
+                        <div class="col-4 text-right">
+                            <router-link
+                                    v-if="addButton"
+                                    :to="addButton.uri"
+                                    tag="div"
+                                    class="btn btn-primary pull-right"
+                                    :disabled="addButton.disabled"
+                                    dusk="createEventButton"
+                            >
+                                <i class="fa fa-plus"></i>
+                            </router-link>
+
+                            <slot name="buttons"></slot>
+                        </div>
+                    </div>
+                </div>
+
             </div>
+
         </div>
 
         <div class="row">
