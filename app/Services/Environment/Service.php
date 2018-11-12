44f3109a23a7fa1fd3f73076a1e636e081a0f0f5
Alexandre Ferreira <alexandreee@gmail.com>
2018-11-12 16:30:19 -0200

    Recarregar dados de pessoa que foi gravada
    Recarregar dados de pessoa que foi gravada
    #197

--- app/Services/Environment/Service.php
+++ app/Services/Environment/Service.php
@@ -73,7 +73,7 @@ class Service
                 'confirmation' => [
                     'enabled' => config('events.confirmation.enabled'),
                 ],
-            ]
+            ],
         ]);
     }
 }
