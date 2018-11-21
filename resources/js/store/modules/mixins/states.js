1b5ab3434d66ebdc97d7981a136fd0f2eb4c2ca4
BMasquio <brunomasquio@gmail.com>
2018-11-21 13:16:02 +0000

    Working on institution and role filter 2

--- resources/js/store/modules/mixins/states.js
+++ resources/js/store/modules/mixins/states.js
@@ -8,6 +8,10 @@ export const common = {
             checkboxes: {
                 filler: false,
             },
+
+            selects: {
+                filler: false,
+            },
         },
 
         links: {
