b4d58235ee899c811abc36c9669c8ea367800f4f
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-11-21 13:49:18 -0200

    Change default font

--- app/Services/PDF/Service.php
+++ app/Services/PDF/Service.php
@@ -8,7 +8,11 @@ class Service
 {
     public function download($html, $fileName = null)
     {
-        return PDF::loadHTML($html)->download($this->makeFileName($fileName));
+        return PDF::setOptions([
+            'defaultFont' => 'Helvetica',
+        ])
+            ->loadHTML($html)
+            ->download($this->makeFileName($fileName));
     }
 
     private function makeFileName($fileName)
