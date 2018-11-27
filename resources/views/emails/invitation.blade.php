5bb3cd6654b96adb72abf94b64680e2794bef118
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-11-27 21:10:10 -0200

    Buttons side by side

--- resources/views/emails/invitation.blade.php
+++ resources/views/emails/invitation.blade.php
@@ -1,13 +1,20 @@
 @component('mail::message')
 {!! $invitation->getViewVariables()['invitation_text'] !!}
 
-@component('mail::button', ['url' => route('invitations.accept', ['event_id' => $invitation->subEvent->event->id, 'sub_event_id' => $invitation->subEvent->id, 'invitation_id' => $invitation->id])])
-Aceitar
-@endcomponent
-
+<table>
+<tr>
+<td>
 @component('mail::button', ['color' => 'error', 'url' => route('invitations.reject', ['event_id' => $invitation->subEvent->event->id, 'sub_event_id' => $invitation->subEvent->id, 'invitation_id' => $invitation->id])])
 Declinar
 @endcomponent
+</td>
+<td>
+@component('mail::button', ['url' => route('invitations.accept', ['event_id' => $invitation->subEvent->event->id, 'sub_event_id' => $invitation->subEvent->id, 'invitation_id' => $invitation->id])])
+Aceitar
+@endcomponent
+</td>
+</tr>
+</table>
 
 Atenciosamente,<br>
 <br>
