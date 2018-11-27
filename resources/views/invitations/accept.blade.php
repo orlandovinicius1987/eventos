8ed4bc02aa73dfa181f51cb19c428590fdb18d03
Breno Trengrouse <brenot@gmail.com>
2018-11-27 19:09:08 +0000

    Request CPF - Accept and Reject -  Style 001

--- resources/views/invitations/accept.blade.php
+++ resources/views/invitations/accept.blade.php
@@ -25,49 +25,55 @@
     <div class="container d-flex h-80">
         <div class="row align-self-center w-100">
             <div class="col-md-8 col-lg-8 mx-auto">
+                <div class="card p-5">
 
-                <div class="py-2 mb-4 text-center">
-                    <h2>{{$invitation->personInstitution->person->name}}</h2>
-                </div>
-
-                @if (isset($errors) && $errors->any())
-                    <div class="alert alert-danger">
-                        @foreach ($errors->all() as $error)
-                            {{ $error }}
-                        @endforeach
+                    <div class="py-2 mb-4 text-center">
+                        <h2>{{$invitation->personInstitution->person->name}}</h2>
                     </div>
-                @endif
 
-                <div class="row justify-content-center text-center">
-                    <form method="POST" action="{{ route('invitations.mark-as-accepted', [$eventId, $subEventId, $invitationId]) }}" >
-                        <input type="hidden" name="eventId" value="{{$eventId}}"/>
-                        <input type="hidden" name="subEventId" value="{{$subEventId}}"/>
-                        <input type="hidden" name="invitationId" value="{{$invitationId}}"/>
-                        <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
-
-                        <div>
-                            <h3>Confirme seu CPF para confirmar seu Convite</h3>
-                            <div class="row mb-3 mt-3 ">
-                                <div class="col-12">
-                                    {{--<label for="cpf">CPF</label>--}}
-                                    <input
-                                            name="cpf"
-                                            id="cpf"
-                                            required="true"
-                                            type="text"
-                                            placeholder="CPF"
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="row mt-3">
-                                <div class="col-12">
-                                    <button class="btn btn-outline-secondary" type="submit">gravar</button>
-                                </div>
-                            </div>
+                    @if (isset($errors) && $errors->any())
+                        <div class="alert alert-danger">
+                            @foreach ($errors->all() as $error)
+                                {{ $error }}
+                            @endforeach
                         </div>
-                    </form>
+                    @endif
+
+                    <div class="row justify-content-center text-center">
+                        <form method="POST" action="{{ route('invitations.mark-as-accepted', [$eventId, $subEventId, $invitationId]) }}" >
+                            <input type="hidden" name="eventId" value="{{$eventId}}"/>
+                            <input type="hidden" name="subEventId" value="{{$subEventId}}"/>
+                            <input type="hidden" name="invitationId" value="{{$invitationId}}"/>
+                            <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+
+                            <div>
+                                <h4 class="mb-3">Confirme seu CPF para confirmar seu Convite</h4>
+                                <div class="row mb-3 mt-3">
+                                    <div class="col-12">
+                                        {{--<label for="cpf">CPF</label>--}}
+                                        <input
+                                                name="cpf"
+                                                id="cpf"
+                                                required="true"
+                                                type="text"
+                                                placeholder="CPF"
+                                                class="form-control"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div class="row mt-4">
+                                    <div class="col-12">
+                                        <button class="btn btn-success btn-block" type="submit">Gravar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
                 </div>
+
+
 
             </div>
         </div>
