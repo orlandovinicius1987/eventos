20ee39c9715604bc5ff9ed4645ef213167d48805
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-11-27 14:15:07 -0200

    Fill uuid

--- app/Data/Models/NotificationLog.php
+++ app/Data/Models/NotificationLog.php
@@ -2,6 +2,8 @@
 
 namespace App\Data\Models;
 
+use Ramsey\Uuid\Uuid;
+
 class NotificationLog extends Base
 {
     /**
@@ -10,4 +12,11 @@ class NotificationLog extends Base
     protected $fillable = ['invitation_id', 'subject', 'destination'];
 
     protected $table = 'notification_log';
+
+    public function save(array $options = [])
+    {
+        $this->uuid = $this->uuid ?? (string) Uuid::uuid4();
+
+        return parent::save($options);
+    }
 }
