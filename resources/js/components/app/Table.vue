e356ca2f75624718bcfd7721b68cd1ccd9f45ede
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-11-29 00:16:10 -0200

    Move layout back to what it was originally (kind of)

--- resources/js/components/app/Table.vue
+++ resources/js/components/app/Table.vue
@@ -1,33 +1,31 @@
 <template>
+    <div class="">
+        <table class="table table-sm table-hover table-borderless table-striped card-body mb-0">
+            <thead>
+                <tr>
+                    <slot name="thead"></slot>
 
-            <div class="">
-                <table class="table table-sm table-hover table-borderless card-body mb-0">
-                    <thead>
-                        <tr>
-                            <slot name="thead"></slot>
+                    <th
+                        v-if="columns"
+                        v-for="column in columns"
+                        :class="is_object(column) ? column.trClass : ''"
+                    >
+                        {{ is_object(column) ? column.title : column }}
+                    </th>
+                </tr>
+            </thead>
 
-                            <th
-                                v-if="columns"
-                                v-for="column in columns"
-                                :class="is_object(column) ? column.trClass : ''"
-                            >
-                                {{ is_object(column) ? column.title : column }}
-                            </th>
-                        </tr>
-                    </thead>
+            <tbody>
+                <slot></slot>
+            </tbody>
+        </table>
 
-           <tbody>
-                        <slot></slot>
-                    </tbody>
-                </table>
-
-                <app-pagination
-                    v-if="pagination"
-                    :pagination="pagination"
-                    @goto-page="$emit('goto-page', $event)"
-                ></app-pagination>
-            </div>
-
+        <app-pagination
+            v-if="pagination"
+            :pagination="pagination"
+            @goto-page="$emit('goto-page', $event)"
+        ></app-pagination>
+    </div>
 </template>
 
 <script>
