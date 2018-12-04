33e8875d2285d26e74ab4a13d67fc75b1da9b3e6
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-12-04 19:02:43 -0200

    Remove debuging rows

--- app/Data/Repositories/Notifications.php
+++ app/Data/Repositories/Notifications.php
@@ -48,8 +48,6 @@ class Notifications extends Repository
 
     public function registerMessageStatus($status, $data)
     {
-        info($data);
-
         if (($message = $this->findByMessageId($data['message_id']))) {
             $message->{$status . '_at'} = $data['timestamp'];
 
@@ -58,8 +56,6 @@ class Notifications extends Repository
             if ($status === 'opened' || $status === 'clicked') {
                 $message->markAsReceived();
             }
-
-            info($message->toArray());
         }
     }
 }
