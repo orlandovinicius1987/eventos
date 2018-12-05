49365ff40889d50ba2db197a9d672ad7f97f8855
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-12-05 16:27:41 -0200

    Generate new events

---
+++ app/Events/InvitationsUpdated.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+
+class InvitationsUpdated extends Event
+{
+    /**
+     * Information about the subEvent update.
+     *
+     * @var string
+     */
+    public $subEvent;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param $subEvent
+     */
+    public function __construct($subEvent)
+    {
+        $this->subEvent = $subEvent;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('sub-event.' . $this->subEvent->id);
+    }
+}
