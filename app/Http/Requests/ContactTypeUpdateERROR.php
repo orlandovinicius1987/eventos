4f39d815405ec6a43e37ea14ff19584ac353bc9e
BMasquio <brunomasquio@gmail.com>
2018-12-05 17:08:27 +0000

    Added CPF mask when accepting invitation

--- app/Http/Requests/ContactTypeUpdateERROR.php
+++ app/Http/Requests/ContactTypeUpdateERROR.php
@@ -21,28 +21,4 @@ class ContactTypeUpdateERROR extends BaseStore
             'contact' => ['required', new Contact()],
         ];
     }
-
-    /**
-     * @return array
-     */
-    public function sanitize()
-    {
-        if (!empty($this->get('contact'))) {
-            $contactType = app(ContactTypesRepository::class)->findById(
-                $this->get('contact_type_id')
-            );
-
-            if (
-                $contactType->code == 'mobile' ||
-                $contactType->code == 'whatsapp' ||
-                $contactType->code == 'phone'
-            ) {
-                $input = $this->all();
-                $phone = $this->get('contact');
-                $input['contact'] = preg_replace('/\D/', '', $phone);
-                $this->replace($input);
-            }
-        }
-        return $this->all();
-    }
 }
