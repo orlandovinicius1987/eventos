1da3ce04e292fcccce981f2237cf15923a73448a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-12-17 19:52:22 -0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,19 @@ pkill node
 echo "Compiling assets to production..."
 npm run production
 
+echo "Commit production files..."
+git add config/version.yml
+git add public/*
+git commit -m "Compile to production"
+
 echo "Clearing cache..."
 php artisan cache:clear
 
-echo "Commiting version.yml file..."
-git add config/version.yml
-git add public/*
-git commit -m "Update version and compile to production"
-
 echo "Absorbing version..."
 php artisan version:absorb
+
+echo "Commiting version.yml file..."
+git add config/version.yml
+git commit -m "Update version"
 
 echo "done."
