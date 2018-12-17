ab59069d623950f06e5a2ca8fbf69cd130680251
Breno Trengrouse <brenot@gmail.com>
2018-12-17 14:39:20 +0000

    Heading change -name +username

--- resources/views/layouts/partials/heading.blade.php
+++ resources/views/layouts/partials/heading.blade.php
@@ -135,7 +135,7 @@
 
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <i class="fas fa-user"></i> {{ Auth::user()->name }} <span class="caret"></span>
+                                    <i class="fas fa-user"></i> {{ Auth::user()->username }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
