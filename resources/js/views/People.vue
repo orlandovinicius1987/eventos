a5bab8c7a40baf4e249057b9c86eed60cfd00931
Breno Trengrouse <brenot@gmail.com>
2018-12-19 15:07:55 +0000

    Style Fix 032 - responsive adjusts PEOPLE2

--- resources/js/views/People.vue
+++ resources/js/views/People.vue
@@ -15,7 +15,7 @@
                             <h1 class="display-4 mb-0">{{ selected.name }}</h1>
                         </div>
 
-                        <div class="col-12 col-lg-4">
+                        <div class="col-4">
                             <img
                                 :src="makePhotoUrl(selected.photoUrl)"
                                 class="img-thumbnail rounded mx-auto d-block mb-2"
