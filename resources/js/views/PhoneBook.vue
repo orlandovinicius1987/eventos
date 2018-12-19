b00b8767e82f7e616d85976e4c357ab6755c0f40
Breno Trengrouse <brenot@gmail.com>
2018-12-19 15:56:12 +0000

    Style Fix 036 - responsive adjusts Phonebook

--- resources/js/views/PhoneBook.vue
+++ resources/js/views/PhoneBook.vue
@@ -29,7 +29,7 @@
         </div>
 
         <div class="row">
-            <div class="col-4">
+            <div class="col-12 col-lg-4">
                 <app-table-panel
                     :title="'Pessoas (' + pagination.total + ')'"
                     :per-page="perPage"
