05f24bbdbef225e8a20ec292b7cc0a2737612878
BMasquio <brunomasquio@gmail.com>
2019-01-09 18:12:20 +0000

    Fixed error lines in ContactTypesForm

--- resources/js/views/ContactTypesForm.vue
+++ resources/js/views/ContactTypesForm.vue
@@ -14,19 +14,23 @@
                     <div class="row">
                         <div class="col-12 mb-3">
                             <app-input
-                                name="name"
-                                label="Nome"
-                                v-model="form.fields.name"
-                                :required="true"
-                                :form="form"
+                                    name="name"
+                                    label="Nome"
+                                    v-model="form.fields.name"
+                                    :required="true"
+                                    :form="form"
                             ></app-input>
+                        </div>
+                    </div>
 
+                    <div class="row">
+                        <div class="col-12 mb-3">
                             <app-input
-                                name="code"
-                                label="Código"
-                                v-model="form.fields.code"
-                                :required="true"
-                                :form="form"
+                                    name="code"
+                                    label="Código"
+                                    v-model="form.fields.code"
+                                    :required="true"
+                                    :form="form"
                             ></app-input>
                         </div>
                     </div>
