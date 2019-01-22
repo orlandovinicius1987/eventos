346fb89ab6dda7b86ca4225b07b845beac6cb3b0
Alexandre Ferreira <alexandreee@gmail.com>
2019-01-22 15:25:27 -0200

    Impedir de uma pessoa se adicionar como assessora dela mesma.

--- resources/js/views/AdvisorsForm.vue
+++ resources/js/views/AdvisorsForm.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="py-2 text-center">
-            <h2>{{ this.mode === 'create' ? 'Criar ' : 'Editar ' }}Assessor1</h2>
+            <h2>{{ this.mode === 'create' ? 'Criar ' : 'Editar ' }}Assessor</h2>
             <h2>&nbsp;{{ form.fields.name ? form.fields.name : '' }}</h2>
         </div>
 
