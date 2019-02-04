c27fada36b6874af19bf16c48c0442b43ae87e9e
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-02-04 11:49:50 -0200

    Refactor populate migrations

--- database/migrations/2018_11_21_000001_populate_sectors.php
+++ database/migrations/2018_11_21_000001_populate_sectors.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Data\Models\Sector;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class PopulateSectors extends Migration
@@ -14,19 +11,6 @@ class PopulateSectors extends Migration
      */
     public function up()
     {
-        Schema::table('populate_sectors', function (Blueprint $table) {
-            foreach (
-                [
-                    ['name' => 'Galeria', 'color' => '#739735'],
-                    ['name' => 'Tribuna de Honra', 'color' => '#997220'],
-                    ['name' => 'Plenário', 'color' => '#224792'],
-                    ['name' => 'Estacionamento', 'color' => '#000000'],
-                ]
-                as $item
-            ) {
-                Sector::create($item);
-            }
-        });
     }
 
     /**
