5f6a58ca9b1279e5ef4844b3acb029de5e622452
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-02-04 11:51:43 -0200

    Add slug

--- app/Data/Models/Topic.php
+++ app/Data/Models/Topic.php
@@ -6,12 +6,21 @@ class Topic extends BaseWithClient
     /**
      * @var array
      */
-    protected $fillable = ['name', 'client_id'];
+    protected $fillable = ['name', 'slug', 'client_id'];
 
     protected $orderBy = ['name' => 'asc'];
 
     public function person_topics()
     {
         return $this->hasMany(PersonTopic::class);
+    }
+
+    public function save(array $options = [])
+    {
+        if (blank($this->slug)) {
+            $this->slug = make_slug($this->name);
+        }
+
+        return parent::save($options);
     }
 }
