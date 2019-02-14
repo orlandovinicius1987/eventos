f85eafffac0581833ae903cc683203d19fb8de36
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-02-14 14:26:47 -0200

    Readd file

---
+++ resources/js/components/app/Textarea.vue
@@ -0,0 +1,25 @@
+<template>
+    <span>
+        <label :for="name" class="mb-0 mt-4">{{ label }}</label>
+
+        <textarea
+            v-bind:value="value"
+            v-on:input="$emit('input', $event.target.value)"
+            class="form-control"
+            :id="name"
+            :required="required"
+            :rows="rows"
+            :cols="cols"
+        ></textarea>
+
+        <small class="text-danger" v-if="form.errors.has(name)">
+            {{ form.errors.get(name) }}
+        </small>
+    </span>
+</template>
+
+<script>
+export default {
+    props: ['value', 'name', 'rows', 'cols', 'form', 'required', 'label'],
+}
+</script>
